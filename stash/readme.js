1958206054f49b5f2498208bbbc8aeaff57225c6
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-03-25 14:00:00 +0200

    back to fraud

--- stash/readme.js
+++ stash/readme.js
@@ -2,17 +2,17 @@ module.exports =
 '# fraud\n\n' +
 '[![build status](http://img.shields.io/travis/chiefbiiko/fraud.svg?style=flat)](http://travis-ci.org/chiefbiiko/fraud) [![AppVeyor Build Status](https://ci.appveyor.com/api/projects/status/github/chiefbiiko/fraud?branch=master&svg=true)](https://ci.appveyor.com/project/chiefbiiko/fraud)\n\n' +
 '***\n\n' +
-'noop skeleton\n\n' +
+'fraud\n\n' +
 '***\n\n' +
 '## Get it!\n\n' + 
 '```\nnpm install --save fraud\n```\n\n' +
 '***\n\n' +
 '## Usage\n\n' + 
-'``` js\n/*...*/\n```\n\n' +
+'``` js\n/* fraud */\n```\n\n' +
 '***\n\n' +
 '## API\n\n' + 
-'### `noop()`\n\n' +
-'Do nothing.\n\n' +
+'### `fraud`\n\n' +
+'fraud\n\n' +
 '***\n\n' +
 '## License\n\n' +
 '[MIT](./license.md)\n'
