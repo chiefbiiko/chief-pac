ed166d6f98f8b9227707f4f1fee0a5154255b3ab
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-06-27 20:13:39 +0200

    assert filenames

--- test.js
+++ test.js
@@ -12,8 +12,22 @@ tape.onFinish(del.sync.bind(null, testDir))
 tape('files are written to designated dir', function (t) {
 
   child.execSync('node cli ' + testDir)
+ 
+  var files = fs.readdirSync(testDir)
 
-  t.is(fs.readdirSync(testDir).length, 7, 'files should have been written')
+  t.is(files.length, 7, 'files should have been written')
+  t.true([ 
+    'appveyor.yml', 
+    '.travis.yml', 
+    'license.md', 
+    'readme.md', 
+    'security.md', 
+    '.gitignore', 
+    'test.js' 
+   ].every(function (musthave) {
+     return files.includes(musthave)
+   }),
+   'got all files')
 
   t.end()
 })
